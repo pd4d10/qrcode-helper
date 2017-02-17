3c3124631f574b58add1aaa94a11408daab98cde
pd4d10 <pd4d10@gmail.com>
2017-02-17 20:20:09 +0800

    Generate QRCode

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,6 +4,21 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeText({ text: '\'Allo' });
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+chrome.contextMenus.create({
+  title: chrome.i18n.getMessage('generate'),
+  contexts: ['page', 'selection', 'link'],
+  onclick(info, tab) {
+    console.log(info)
+
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    }, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, { info })
+    })
+  }
+});
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,31 @@
-'use strict';
+console.log('qrcode')
 
-console.log('\'Allo \'Allo! Content script');
+// import QRCode from '../bower_components/qrcode.js/lib/qrcode'
+
+const DOM_ID = 'qrcode'
+
+const $container = document.createElement('div')
+$container.style.position = 'fixed'
+$container.style.left = '50%'
+$container.style.top = '50%'
+$container.id = DOM_ID
+document.body.appendChild($container)
+
+chrome.runtime.onMessage.addListener(message => {
+  console.log(message)
+
+  const { info } = message
+  if (typeof info.selectionText === 'string') {
+    console.log(info)
+    // require('../lib/qrcode', qrcode => {
+      // new window.QRCode(DOM_ID, info.selectionText)
+    // })
+
+    // https://github.com/kazuhikoarase/qrcode-generator/blob/master/js/sample.js
+    var qr = qrcode(4, 'M');
+    qr.addData(info.selectionText);
+    qr.make();
+
+    $container.innerHTML = qr.createImgTag()
+  }
+})
