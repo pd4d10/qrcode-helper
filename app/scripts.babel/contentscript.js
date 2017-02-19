b2c71a47932e63864404bfa0b2fe9c754e9fed0d
pd4d10 <pd4d10@gmail.com>
2017-02-19 15:22:32 +0800

    Use dialog element

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,15 +1,24 @@
 console.log('qrcode')
 
-// import QRCode from '../bower_components/qrcode.js/lib/qrcode'
+const DOM_ID = 'qrcode_qrcode'
 
-const DOM_ID = 'qrcode'
+function showDialog() {
+  if (!document.getElementById(DOM_ID)) {
+    const $dialog = document.createElement('dialog')
+    $dialog.id = DOM_ID
+    document.body.appendChild($dialog)
+  }
 
-const $container = document.createElement('div')
-$container.style.position = 'fixed'
-$container.style.left = '50%'
-$container.style.top = '50%'
-$container.id = DOM_ID
-document.body.appendChild($container)
+  document.getElementById(DOM_ID).showModal()
+}
+
+function hideDialog() {
+  try {
+    document.getElementById(DOM_ID).close()
+  } catch (err) {
+    // If no element found, do nothing
+  }
+}
 
 chrome.runtime.onMessage.addListener(message => {
   console.log(message)
@@ -17,20 +26,19 @@ chrome.runtime.onMessage.addListener(message => {
   switch (message.type) {
     case 'generate': {
       if (typeof message.info.selectionText === 'string') {
-        // require('../lib/qrcode', qrcode => {
-          // new window.QRCode(DOM_ID, info.selectionText)
-        // })
-
         // https://github.com/kazuhikoarase/qrcode-generator/blob/master/js/sample.js
         var qr = qrcodeGenerator(4, 'M');
         qr.addData(message.info.selectionText);
         qr.make();
 
-        $container.innerHTML = qr.createImgTag()
+        showDialog()
+        document.getElementById(DOM_ID).innerHTML = qr.createImgTag()
       }
+      break
     }
     case 'read': {
       alert(message.text)
+      break
     }
     default:
       break
