ae5663853f8108f87127a3eeee00d32120124088
Rongjian Zhang <pd4d10@gmail.com>
2019-01-04 15:14:56 +0800

    build: drop console at production

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 const path = require('path')
 const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,10 +1,24 @@
+// @ts-check
 const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 const devConfig = require('./webpack.config')
 
 /** @type {webpack.Configuration} */
 const config = {
   ...devConfig,
   mode: 'production',
+  watch: false,
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
 }
 
 module.exports = config
