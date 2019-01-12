5b0eee9ffeba8333f22d15965521245da903ca67
Rongjian Zhang <pd4d10@gmail.com>
2019-01-12 18:33:32 +0800

    feat: use non-persistant background to save memory

--- src/background.ts
+++ src/background.ts
@@ -18,33 +18,42 @@ chrome.browserAction.onClicked.addListener(tab => {
   }
 })
 
-// generate from selection
-chrome.contextMenus.create({
-  title: chrome.i18n.getMessage('generate'),
-  contexts: ['selection'],
-  onclick(info) {
-    console.log(info)
-    if (info.selectionText) {
-      openCodePopup(info.selectionText)
-    }
-  },
+chrome.runtime.onInstalled.addListener(() => {
+  // generate from selection
+  chrome.contextMenus.create({
+    id: 'generate',
+    title: chrome.i18n.getMessage('generate'),
+    contexts: ['selection'],
+  })
+
+  // read from image
+  chrome.contextMenus.create({
+    id: 'read',
+    title: chrome.i18n.getMessage('read'),
+    contexts: ['image'],
+  })
 })
 
-// read from image
-chrome.contextMenus.create({
-  title: chrome.i18n.getMessage('read'),
-  contexts: ['image'],
-  onclick(info) {
-    console.log(info)
-    if (info.srcUrl) {
-      chrome.windows.create({
-        url: chrome.extension.getURL(
-          'dist/decode.html?url=' + encodeURIComponent(info.srcUrl),
-        ),
-        type: 'popup',
-        width: 480,
-        height: 480,
-      })
-    }
-  },
+// make context menu works with non-persistent background
+// https://stackoverflow.com/a/26246735
+chrome.contextMenus.onClicked.addListener(info => {
+  console.log(info)
+
+  switch (info.menuItemId) {
+    case 'generate':
+      if (info.selectionText) {
+        openCodePopup(info.selectionText)
+      }
+    case 'read':
+      if (info.srcUrl) {
+        chrome.windows.create({
+          url: chrome.extension.getURL(
+            'dist/decode.html?url=' + encodeURIComponent(info.srcUrl),
+          ),
+          type: 'popup',
+          width: 480,
+          height: 480,
+        })
+      }
+  }
 })
